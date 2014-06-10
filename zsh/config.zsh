b29161bdfefc57b1624fd9b76fbb9c9d550a0f27
pawcik <pawcik@gmail.com>
2014-06-10 15:06:09 +0200

    add history substring plugin, and share zsh history

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -8,6 +8,13 @@ autoload -U $ZSH/zsh/functions/*(:t)
 HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
+setopt extended_history # save timestamp and runtime information
+setopt inc_append_history #To save every command before it is executed 
+setopt hist_verify # perform history expansion and reload the line into the editing buffer
+setopt hist_expire_dups_first # allow dups, but expire old ones when I hit HISTSIZE
+setopt hist_ignore_dups # ignore duplication command history list
+setopt hist_ignore_space # Remove command lines from the history list when the first character on the line is a space
+setopt share_history #To retrieve the history file everytime history is called upon.
 
 setopt NO_BG_NICE # don't nice background tasks
 setopt NO_HUP
---
+++ zsh/plugins/zsh-syntax_zsh-history-substring-search/install_plugin.zsh
@@ -0,0 +1,30 @@
+#is should be installed after highlight plugin
+CWD=$(dirname $0)
+source $CWD/zsh-history-substring-search/zsh-history-substring-search.zsh
+
+# bind UP and DOWN arrow keys
+zmodload zsh/terminfo
+# stolen from https://github.com/robbyrussell/oh-my-zsh/blob/master/lib/key-bindings.zsh
+# Make sure that the terminal is in application mode when zle is active, since
+# only then values from $terminfo are valid
+if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
+  function zle-line-init() {
+    echoti smkx
+  }
+  function zle-line-finish() {
+    echoti rmkx
+  }
+  zle -N zle-line-init
+  zle -N zle-line-finish
+fi
+
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
+
+# bind P and N for EMACS mode
+bindkey -M emacs '^P' history-substring-search-up
+bindkey -M emacs '^N' history-substring-search-down
+
+# bind k and j for VI mode
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
