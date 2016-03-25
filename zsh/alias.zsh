63f5961572132aad39607dacdc025207bc889b27
Pawel Jedruch <pawcik@gmail.com>
2016-03-25 15:27:12 +0100

    Add hex two decimal alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -3,3 +3,7 @@ alias la="ls -al"
 alias grep="grep --color=auto"
 alias openapp="open -b com.jetbrains.AppCode"
 alias openvlc="open -b org.videolan.vlc"
+
+#hex conversion
+alias hex2dec="printf '%d\n'"
+alias dec2hex="printf '%x\n'"
